746744190d46e4f36fa6a78a65dc0c7d69a7ed43
kcomputer <emil.ildarovich@gmail.com>
2013-11-28 18:13:27 +0400

    Initial Commit

---
+++ SoundsBetterWithReverb/SBWRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBWRAppDelegate.h
+//  SoundsBetterWithReverb
+//
+//  Created by Emil on 11/28/13.
+//  Copyright (c) 2013 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBWRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SoundsBetterWithReverb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SoundsBetterWithReverb
+//
+//  Created by Emil on 11/28/13.
+//  Copyright (c) 2013 Emil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SBWRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SBWRAppDelegate class]));
+    }
+}
